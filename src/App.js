bed161d7b3a8c94655cbf7c0c767df7cb92abc62
Omkar Patil <omkar@flhealth.in>
2024-03-28 20:56:07 +0530

    feat(Component): :sparkles: Setup router and basic structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/organization' element={<Home />} />
+        <Route path='/assets' element={<Home />} />
+        <Route path='/trade' element={<Home />} />
+        <Route path='/history' element={<Home />} />
+        <Route path='/wallet' element={<Home />} />
+      </Routes>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      colors:{
+        primaryColor: '#22c55e'
+      }
+    },
+  },
+  plugins: [],
+}
